d7d4d29ee16f2506fdeda9e002e14d3d2700f73b
Natalia Colmenares <38693615+nataliacolm@users.noreply.github.com>
2021-02-17 13:07:18 -0500

    adds main menu

---
+++ projects/Manifesting Destiny/Assets/MainMenuAssets/Scripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+      // Game scene is one index away from current main menu scene in the build index.
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
