0cf16ec2440636fae9cd42ef72ed79afa212495f
AedanDeyto22 <adeyto722@knights.ucf.edu>
2021-02-20 14:21:50 -0500

    Added The Indicator

---
+++ projects/Manifesting Destiny/Assets/Scripts/Indicator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using TMPro;
+
+public class Indicator : MonoBehaviour
+{
+    private static int resourceFood = 0;
+    private static int resourceGold = 0;
+    private static int resourceWood = 0;
+
+    public TextMeshProUGUI resourceAmountFood;
+    public TextMeshProUGUI resourceAmountGold;
+    public TextMeshProUGUI resourceAmountWood;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          resourceFood = Resources.getFood();
+          resourceGold = Resources.getGold();
+          resourceWood = Resources.getWood();
+
+          resourceAmountFood.text = Convert.ToString(resourceFood);
+          resourceAmountGold.text = Convert.ToString(resourceGold);
+          resourceAmountWood.text = Convert.ToString(resourceWood);
+
+          Debug.Log(resourceFood);
+          Debug.Log(resourceGold);
+          Debug.Log(resourceWood);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          resourceFood = Resources.getFood();
+          resourceGold = Resources.getGold();
+          resourceWood = Resources.getWood();
+
+          resourceAmountFood.text = Convert.ToString(resourceFood);
+          resourceAmountGold.text = Convert.ToString(resourceGold);
+          resourceAmountWood.text = Convert.ToString(resourceWood);
+
+          Debug.Log(resourceFood);
+          Debug.Log(resourceGold);
+          Debug.Log(resourceWood);
+    }
+}
