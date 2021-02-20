a27fd3828991b6855ea580829f47bcad98b4d3bc
AedanDeyto22 <adeyto722@knights.ucf.edu>
2021-02-20 11:51:54 -0500

    Added Scripts Folder

---
+++ projects/Test Project/Assets/Indicator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Indicator : MonoBehaviour
+{
+    private static int resourceFood = 0;
+    private static int resourceGold = 0;
+    private static int resourceWood = 0;
+
+    public TextMeshProUGUI resourceAmountFood;
+    public TextMeshProUGUI resourceAmountGold;
+    public TextMeshProUGUI resourceAmountWood;
+    // Start is called before the first frame update
+    void Start()
+    {
+          resourceAmountFood.text = "0";
+          resourceAmountGold.text = "0";
+          resourceAmountWood.text = "0";
+          Debug.Log(resourceFood);
+          Debug.Log(resourceGold);
+          Debug.Log(resourceWood);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          resourceAmountFood.text = "50";
+          resourceAmountGold.text = "50";
+          resourceAmountWood.text = "50";
+          Debug.Log(resourceFood);
+          Debug.Log(resourceGold);
+          Debug.Log(resourceWood);
+    }
+}
