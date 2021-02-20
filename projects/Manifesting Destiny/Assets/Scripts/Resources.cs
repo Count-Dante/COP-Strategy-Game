17f58f2428bf18de10ba15d7c8e34156a8b02afb
Kipikash-Git <70029455+Kipikash-Git@users.noreply.github.com>
2021-02-20 13:28:40 -0500

    Broken gather wood

---
+++ projects/Manifesting Destiny/Assets/Scripts/Resources.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources : MonoBehaviour
+{
+
+      private static int gold = 0;
+      private static int wood = 0;
+      private static int food = 0;
+
+      public static int getGold()
+      {
+            return gold;
+      }
+
+      public static void setGold(int amount)
+      {
+            gold = amount;
+      }
+
+      public static int getWood()
+      {
+            return wood;
+      }
+
+      public static void setWood(int amount)
+      {
+            wood = amount;
+      }
+
+      public static int getFood()
+      {
+            return food;
+      }
+
+      public static void setFood(int amount)
+      {
+            food = amount;
+      }
+
+      // Get random number from 1 - 100 (inclusive).
+      public static int randomNumberGenerator()
+      {
+            return UnityEngine.Random.Range(1,100);
+      }
+
+      void Update()
+      {
+            Debug.Log(wood);
+      }
+
+
+
+}
