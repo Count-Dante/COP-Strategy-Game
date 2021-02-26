c8eae95b3139c0df7c2e8d4f912a7901a337a9a5
Grant Allan <45442774+Grant-Allan@users.noreply.github.com>
2021-02-26 13:08:01 -0500

    Quit Button Added

---
+++ projects/Manifesting Destiny/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        // If it's in the Unity Editor, this will close it
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        // If it's being run as an application, this will quit the application
+        Application.Quit();
+    }
+}
