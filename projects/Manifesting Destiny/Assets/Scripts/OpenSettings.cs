d41955cdc04c96ec040dda0c35b44b9adff9d98b
Grant Allan <45442774+Grant-Allan@users.noreply.github.com>
2021-02-26 11:34:10 -0500

    Adding exit game and settings options

---
+++ projects/Manifesting Destiny/Assets/Scripts/OpenSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSettings : MonoBehaviour
+{
+    public void OpenSettingsMenu()
+    {
+        // Open the Settings scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
