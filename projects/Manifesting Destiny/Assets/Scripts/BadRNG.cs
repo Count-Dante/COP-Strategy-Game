924b6af23b3782e32fa60462c380d7d56dbad0c5
unknown <rblack01@knights.ucf.edu>
2021-02-27 21:30:39 -0500

    Creation of BadRNG - creates random negative events.

---
+++ projects/Manifesting Destiny/Assets/Scripts/BadRNG.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadRNG : MonoBehaviour
+{
+
+    public GameObject PostRNG;
+
+    public void whichEvent()
+    {
+    
+        int wood = Resources.getWood();
+        int gold = Resources.getGold();
+        int food = Resources.getFood();
+
+        int curEvent = 0;
+        //   int defense = wood;
+        int defense = wood + gold + food;
+
+        PostRNG.SetActive(true);
+
+        int which = UnityEngine.Random.Range(1, 3);
+
+        switch (which)
+        {
+            case 1:
+                curEvent = 1;
+                break;
+            case 2:
+                curEvent = 2;
+                break;
+            case 3:
+                curEvent = 3;
+                break;
+        }
+
+        if (curEvent == 1 && defense - 15 >= 0)
+            defense -= 15;
+        
+
+        else if (curEvent == 2 && defense - 10 >= 0)
+            defense -= 10;
+
+        else if (curEvent == 3 && defense - 5 >= 0)
+            defense -= 5;
+        
+
+        else
+            curEvent = 4;
+
+        //PostRNG.SetActive(true);
+        PostRNG.transform.GetChild(curEvent).gameObject.SetActive(true);
+
+        Debug.Log("Current event is " + curEvent);
+        Debug.Log("defense is " + defense);
+        Debug.Log("expansion is ");
+
+
+        // reutrn cur
+
+    }   
+}
