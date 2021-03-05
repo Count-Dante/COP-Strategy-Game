4162582630c7b16ce83c77c6c0c10ed5e17bb77e
Grant Allan <45442774+Grant-Allan@users.noreply.github.com>
2021-03-05 12:03:58 -0500

    Background Music and Button Sounds

---
+++ projects/Manifesting Destiny/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void MusicVolume(float music_volume)
+    {
+        Debug.Log(music_volume);
+        audioMixer.SetFloat("music", music_volume);
+    }
+
+    public void GameSoundsVolume(float game_sounds_volume)
+    {
+        Debug.Log(game_sounds_volume);
+        audioMixer.SetFloat("game_sounds", game_sounds_volume);
+    }
+}
+
