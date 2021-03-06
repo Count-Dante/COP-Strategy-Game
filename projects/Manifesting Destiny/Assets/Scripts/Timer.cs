08a060e2f20cc9b498643e13cf32a0389b05b2d8
Kipikash-Git <kipikash@gmail.com>
2021-03-06 18:46:54 -0500

    Timer

---
+++ projects/Manifesting Destiny/Assets/Scripts/Timer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+      public float totalTime = 25f;
+      public static float remainingTime;
+      public static bool timerEnabled;
+      public GameObject timerText;
+      Image timerBar;
+
+      // Start is called before the first frame update
+      void Start()
+      {
+            startTimer();
+            timerText.SetActive(false);
+            timerBar = GetComponent<Image>();
+            remainingTime = totalTime;
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+            if (remainingTime > 0 && timerEnabled == true)
+            {
+                  remainingTime -= Time.deltaTime;
+                  timerBar.fillAmount = remainingTime / totalTime;
+            }
+
+            else if (timerEnabled == false)
+            {
+            }
+
+            else
+            {
+                  timerText.SetActive(true);
+                  Time.timeScale = 0;
+            }
+      }
+
+      public void pauseTimer()
+      {
+            timerEnabled = false;
+      }
+
+      public void startTimer()
+      {
+            timerEnabled = true;
+      }
+
+}
