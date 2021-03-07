00e6027d22747b084ecd31fd0e180c6aacfa7762
AedanDeyto22 <adeyto722@knights.ucf.edu>
2021-03-07 14:36:36 -0500

    Added a Manual and Automated Test

---
+++ projects/Manifesting Destiny/Assets/Editor/DefenseCalculatorTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class DefenseCalculatorTest
+{
+    [Test]
+    public void getCurrentDefensePoints_Test()
+    {
+        DefenseController.food = 2;
+        DefenseController.wood = 2;
+        DefenseController.gold = 1;
+        float expectedPoints = (2 * 3) + (2 * 3) + (1 * 5);
+
+        float points = DefenseBar.getCurrentDefensePoint();
+
+        Assert.That(points, Is.EqualTo(expectedPoints));
+    }
+}
