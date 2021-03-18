69cb00f5d4d5c3cd630dfff38922a4d4fbae6505
Grant Allan <45442774+Grant-Allan@users.noreply.github.com>
2021-03-18 12:26:47 -0400

    Tutorial Level

--- projects/Manifesting Destiny/Assets/Scripts/StartGame.cs
+++ projects/Manifesting Destiny/Assets/Scripts/StartGame.cs
@@ -7,7 +7,8 @@ public class StartGame : MonoBehaviour
 {
     public void PlayGame ()
     {
-      // Game scene is one index away from current main menu scene in the build index.
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("FirstSettlement");
     }
 }
+
+
