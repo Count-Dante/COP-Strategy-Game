cda96f038e9b7077a63fa2fd01adf659157491bc
Kipikash-Git <70029455+Kipikash-Git@users.noreply.github.com>
2021-03-18 21:50:14 -0400

    Dynamic Object Creation Draft
    DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT DRAFT

---
+++ projects/Manifesting Destiny/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+      public GameObject go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          GameObject enemy = Instantiate(go, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+          enemy.transform.SetParent (GameObject.FindGameObjectWithTag("MainCamera").transform, true);
+          enemy.transform.SetParent (GameObject.FindGameObjectWithTag("Canvas").transform, true);
+
+    }
+}
